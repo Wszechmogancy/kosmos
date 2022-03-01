980434e8542b1d0456af7c376c8c9fff11e7307d
JOHNPIX <janugabuga@gmail.com>
2022-03-01 21:43:59 +0100

    dodanie pierwszyc plików

---
+++ script.js
@@ -0,0 +1,8 @@
+const menu = document.getElementById("active");
+const t_powitalny = document.getElementById("menu");
+
+menu.addEventListener("click", function puf {
+    if (menu = checked) {
+        t_powitalny.style.visibility = hidden;
+    }
+})
